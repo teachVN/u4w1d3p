80058c76699470ada302d9810e65b109420cb438
teachVN <vinnap1976@gmail.com>
2024-04-18 10:07:48 +0200

    u4w1d3p

--- src/Esercizio3/Carrello.java
+++ src/Esercizio3/Carrello.java
@@ -2,4 +2,44 @@ package Esercizio3;
 
 public class Carrello {
     private Cliente cliente;
+
+    private Articolo[] articoli;
+
+    private double costoTotaleArticoli;
+
+    public Carrello(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Articolo[] getArticoli() {
+        return articoli;
+    }
+
+    public void setArticoli(Articolo[] articoli) {
+        this.articoli = articoli;
+    }
+
+    public double getCostoTotaleArticoli() {
+        costoTotaleArticoli=calcolaCostoTotale();
+        return costoTotaleArticoli;
+    }
+
+    private double calcolaCostoTotale(){
+        double somma=0;
+
+        for(int i=0; i< articoli.length; i++){
+            double prezzo = articoli[i].getPrezzo();
+            somma = somma + prezzo;
+        }
+
+        return somma;
+    }
 }
--- src/Esercizio3/UsaCarrello.java
+++ src/Esercizio3/UsaCarrello.java
@@ -1,4 +1,27 @@
 package Esercizio3;
 
 public class UsaCarrello {
+
+    public static void main(String[] args) {
+        Cliente cliente = new Cliente("23432", "Roberto", "Sada",
+                "email@email.it", "2023-05-06");
+
+        Articolo a1 = new Articolo("23", "matita", 0.5, 100);
+        Articolo a2 = new Articolo("3", "gomma", 0.5, 100);
+        Articolo a3 = new Articolo("2", "penna", 1, 100);
+
+        Articolo[] articoli = new Articolo[3];
+        articoli[0]=a1;
+        articoli[1]=a2;
+        articoli[2]=a3;
+
+        Carrello carrello = new Carrello(cliente);
+
+        carrello.setArticoli(articoli);
+
+        System.out.println(carrello.getCostoTotaleArticoli());
+
+    }
+
+
 }
